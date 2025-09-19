730ddb8d7ca3e2955cfa6ba2366127e73f022a65
BrunoMarcondesSENAI <brunin.marcondes17@gmail.com>
2025-09-19 11:37:54 -0300

    Update ConstrutorConsulta.java

--- ativs/ConstrutorConsulta.java
+++ ativs/ConstrutorConsulta.java
@@ -3,14 +3,14 @@ package aula7be.ativs;
 public class ConstrutorConsulta {
 	public static void main(String[] args) {
 		Consulta Um = new Consulta("10/12/2025");
-		Consulta Dois = new Consulta();
-		Consulta Tres = new Consulta("6, 10, 20", "Jorge", "João");
+		Consulta Dois = new Consulta("15/08/2002", "Jéssica", "Josué");
+		Consulta Tres = new Consulta("6/10/20", "Jorge", "João");
 		
 		System.out.println(Um.getData());
 		
 		System.out.println("");
 		
-		System.out.println(Dois.getNomeDents());
+		System.out.println(Dois.getData() + " " + Dois.getNomePcnte() + " " + Dois.getNomeDents());
 		
 		System.out.println("");
 		
